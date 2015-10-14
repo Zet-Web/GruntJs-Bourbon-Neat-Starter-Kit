6e9309d5a21c981c63eff00b1a456953a0561a5d
Yahya Zini <yahyazini@gmail.com>
2015-10-13 23:57:21 -0400

    Clean markup.

--- inc/html-end.php
+++
@@ -1,8 +0,0 @@
-        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-        <script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.5.7/slick.min.js"></script>
-        <!--PROD-->
-<!--        <script src="<?php echo $base; ?>assets/js/functions.min.js"></script>-->
-       <!--Dev-->
-        <script src="<?php echo $base; ?>assets/js/functions.js"></script>
-    </body>
-</html>
